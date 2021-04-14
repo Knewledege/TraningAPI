1f14ff021bb0c647f1b9732d564bc681049749fb
高橋慧 <takahashi@MacBook-Pro.local>
2021-04-14 10:56:38 +0900

    Initial Commit

---
+++ TraningAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TraningAPI
+//
+//  Created by 高橋慧 on 2021/04/14.
+//  Copyright © 2021 k-takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
